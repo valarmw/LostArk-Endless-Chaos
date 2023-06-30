b79878582a4feb9ebcc5c14e0c4a395ceea5484b
valarmw <32943111+valarmw@users.noreply.github.com>
2023-06-29 17:49:04 -0700

    test.py

--- test.py
+++ test.py
@@ -1,4 +1,26 @@
 import pyautogui
+import pydirectinput
+import time
+import random
+import math
+import argparse
+from datetime import date
+
+
+def sleep(min, max):
+    sleepTime = random.randint(min, max) / 1000.0
+    if sleepTime < 0:
+        return
+    time.sleep(sleepTime)
+
+
+sleep(2000, 2300)
+# pydirectinput.press("`")
+
+
 
 while True:
     print(pyautogui.displayMousePosition())
+    i = input("Enter text (or Enter to quit): ")
+    if not i:
+        break
